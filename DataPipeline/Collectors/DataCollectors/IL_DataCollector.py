992a49050e5b7e7701410f3068071263e56d57fb
AyalSwaid <ayalsweed5@gmail.com>
2024-02-26 02:03:02 +0200

    IL some attempts

--- DataPipeline/Collectors/DataCollectors/IL_DataCollector.py
+++ DataPipeline/Collectors/DataCollectors/IL_DataCollector.py
@@ -1,4 +1,6 @@
 from DataCollector import DataCollector
+import requests as reqs
+from bs4 import BeautifulSoup as bs
 
 
 class IL_DataCollector(DataCollector):
@@ -6,9 +8,16 @@ class IL_DataCollector(DataCollector):
         super(IL_DataCollector, self).__init__(batch_size)
     
     
-    def get_debates():
-        pass
+    def get_debates(self):
+        url = 'https://knesset.gov.il/Odata/ParliamentInfo.svc/KNS_CommitteeSession()/'
+        url = 'https://knesset.gov.il/Odata/ParliamentInfo.svc/KNS_Committee(926)?expand=KNS_ComitteeSessions'
+        resp = reqs.get(url)
 
+        soup = bs(resp.content, 'xml')
+        first_element = soup.find('entry').find('properties')
+
+        print(first_element)
+        print(first_element.find_all('SessionUrl'))
 
     def get_votes():
         pass
@@ -20,3 +29,8 @@ class IL_DataCollector(DataCollector):
 
     def get_bills():
         pass
+
+
+if __name__ == "__main__":
+    a = IL_DataCollector(20)
+    a.get_debates()
